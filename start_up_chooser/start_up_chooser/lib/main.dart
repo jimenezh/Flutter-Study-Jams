7de6a736ee3e4108ad0e93ea08e43aafccaca90e
Sheila Jimenez <jimenezh@bu.edu>
2020-10-29 19:35:01 -0400

    Final code

---
+++ start_up_chooser/start_up_chooser/lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+final dummySnapshot = [
+  {"name": "GreatHay", "votes": 15},
+  {"name": "RearWorks", "votes": 14},
+  {"name": "FarmTeam", "votes": 11},
+  {"name": "CheapStay", "votes": 10},
+  {"name": "StraightCourse", "votes": 1},
+];
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Start Up Names Chooser',
+      home: FutureBuilder(
+          future: _initialization,
+          builder: (context, snapshot) {
+            // Check for errors
+            if (snapshot.hasError) {
+              print(snapshot.toString());
+              return Center(
+                  child: Text(
+                snapshot.toString(),
+                style: TextStyle(fontSize: 16),
+              ));
+            }
+            // Once complete, show your application
+            if (snapshot.connectionState == ConnectionState.done) {
+              return MyHomePage();
+            }
+            // Otherwise, show something whilst waiting for initialization to complete
+            return CircularProgressIndicator();
+          }),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Baby Name Votes')),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('name').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildList(context, snapshot.data);
+      },
+    );
+  }
+
+  Widget _buildList(BuildContext context, QuerySnapshot snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children:
+          snapshot.docs.map((name) => _buildListItem(context, name)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, QueryDocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.votes.toString()),
+          onTap: () => record.increment(),
+        ),
+      ),
+    );
+  }
+}
+
+class Record {
+  final String name;
+  final int votes;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['votes'] != null),
+        name = map['name'],
+        votes = map['votes'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data(), reference: snapshot.reference);
+
+  increment() {
+    this.reference.update({'votes': FieldValue.increment(1)});
+  }
+
+  @override
+  String toString() => "Record<$name:$votes, ref:$reference>";
+}
